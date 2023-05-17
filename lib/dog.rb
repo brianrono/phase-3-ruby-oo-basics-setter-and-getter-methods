8ca9a993262795bb08c14da88a3b3d1bc07808cc
brianrono <brianrono16@gmail.com>
2023-05-17 18:24:33 +0300

    Complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+    end
+    def initialize
+        @breed = nil
+    end
+    def breed
+        @breed
+    end
+    def breed=(new_breed)
+        @breed = new_breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
 class Person
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+    end
+    def initialize
+        job = nil
+    end
+    def job
+        @job
+    end
+    def job=(new_job)
+        @job = new_job
+    end
 
 end
